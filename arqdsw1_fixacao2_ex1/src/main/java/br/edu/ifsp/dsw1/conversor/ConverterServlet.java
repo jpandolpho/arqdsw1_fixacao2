0f39099380af6b1da9f36c46cf1a1e459faf6f1c
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-10-25 11:24:00 -0300

    Criação do método doPost, processando o formulário.

--- arqdsw1_fixacao2_ex1/src/main/java/br/edu/ifsp/dsw1/conversor/ConverterServlet.java
+++ arqdsw1_fixacao2_ex1/src/main/java/br/edu/ifsp/dsw1/conversor/ConverterServlet.java
@@ -11,8 +11,13 @@ import java.io.PrintWriter;
 @WebServlet("/main.do")
 public class ConverterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private Converter converter;
+	
+	public ConverterServlet() {
+		converter = new Converter();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		var out = response.getWriter();
 		out.println("<!DOCTYPE html>\n"
 				+ "<html lang=\"en\">\n"
@@ -28,16 +33,16 @@ public class ConverterServlet extends HttpServlet {
 				+ "        <form action=\"main.do\" method=\"post\">\n"
 				+ "            <div class=\"mb-3\">\n"
 				+ "            <label for=\"temperatura\" class=\"form-label\">Temperatura</label>\n"
-				+ "            <input class=\"form-control\" type=\"number\" name=\"temperatura\" id=\"temperatura\" placeholder=\"Temperatura\" aria-label=\"temperatura\">\n"
+				+ "            <input class=\"form-control\" type=\"number\" name=\"text_temperature\" id=\"temperatura\" placeholder=\"Temperatura\" aria-label=\"temperatura\">\n"
 				+ "            </div>\n"
 				+ "            <div class=\"form-check\">\n"
-				+ "                <input class=\"form-check-input\" type=\"radio\" name=\"unidade\" id=\"celsius\" checked>\n"
+				+ "                <input class=\"form-check-input\" type=\"radio\" name=\"radio_scale\" id=\"celsius\" value=\"celsius\" checked>\n"
 				+ "                <label class=\"form-check-label\" for=\"celsius\">\n"
 				+ "                Celsius\n"
 				+ "                </label>\n"
 				+ "            </div>\n"
 				+ "            <div class=\"form-check\">\n"
-				+ "                <input class=\"form-check-input\" type=\"radio\" name=\"unidade\" id=\"fahrenheit\">\n"
+				+ "                <input class=\"form-check-input\" type=\"radio\" name=\"radio_scale\" id=\"fahrenheit\" value=\"fahrenheit\">\n"
 				+ "                <label class=\"form-check-label\" for=\"fahrenheit\">\n"
 				+ "                Fahrenheit\n"
 				+ "                </label>\n"
@@ -50,8 +55,66 @@ public class ConverterServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String temperature = request.getParameter("text_temperature");
+		String scale = request.getParameter("radio_scale");
+		
+		double temp;
+		try {
+			temp = Double.parseDouble(temperature);
+		}catch(NumberFormatException e) {
+			temp = 0;
+		}
+		converter.setTemperatura(temp);
+		
+		if(scale.equals("fahrenheit")) {
+			converter.setStrategy(new ToFahrenheit());
+		}else{
+			converter.setStrategy(new ToCelsius());
+		}
+		try(var out = response.getWriter()){
+			out.println("<!DOCTYPE html>\n"
+					+ "<html lang=\"en\">\n"
+					+ "<head>\n"
+					+ "    <meta charset=\"UTF-8\">\n"
+					+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+					+ "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">\n"
+					+ "    <title>Conversor de Temperatura</title>\n"
+					+ "</head>\n"
+					+ "<body>\n"
+					+ "    <div class=\"container-sm\">\n"
+					+ "    	<h1>Conversor de Temperatura</h1>\n"
+					+ "        <form action=\"main.do\" method=\"post\">\n"
+					+ "            <div class=\"mb-3\">\n"
+					+ "            <label for=\"temperatura\" class=\"form-label\">Temperatura</label>\n"
+					+ "            <input class=\"form-control\" type=\"number\" name=\"text_temperature\" id=\"temperatura\" placeholder=\"Temperatura\" aria-label=\"temperatura\">\n"
+					+ "            </div>\n"
+					+ "            <div class=\"form-check\">\n"
+					+ "                <input class=\"form-check-input\" type=\"radio\" name=\"radio_scale\" id=\"celsius\" checked>\n"
+					+ "                <label class=\"form-check-label\" for=\"celsius\">\n"
+					+ "                Celsius\n"
+					+ "                </label>\n"
+					+ "            </div>\n"
+					+ "            <div class=\"form-check\">\n"
+					+ "                <input class=\"form-check-input\" type=\"radio\" name=\"radio_scale\" id=\"fahrenheit\">\n"
+					+ "                <label class=\"form-check-label\" for=\"fahrenheit\">\n"
+					+ "                Fahrenheit\n"
+					+ "                </label>\n"
+					+ "            </div>\n"
+					+ "            <button type=\"submit\" class=\"btn btn-primary\">Submit</button>\n"
+					+ "      </form>\n<br>\n"
+					+ "      <div class=\"card\">\n"
+					+ "        <div class=\"card-header\">\n"
+					+ "          Convertido\n"
+					+ "        </div>\n"
+					+ "        <div class=\"card-body\">\n"
+					+ "          <h5 class=\"card-title\">Conversão realizada</h5>\n"
+					+ "          <p class=\"card-text\">"+converter.getTemperatura()+"=="+converter.converter()+"</p>\n"
+					+ "        </div>\n"
+					+ "      </div>\n"
+					+ "    </div>\n"
+					+ "</body>\n"
+					+ "</html>");
+		}
 	}
 
 }
